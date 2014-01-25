fe25a91d119e391a09f8cade2e0b120d43708a2a
mildmojo <mildmojo@users.noreply.github.com>
2014-01-25 18:02:06 -0500

    Adds Unity script assets.

---
+++ assets/scripts/Singleton.js
@@ -0,0 +1,20 @@
+#pragma strict
+
+/* Singleton pattern
+ *
+ * GameObjects with this script attached won't be loaded more than once in a
+ * scene. Handy with `DontDestroyOnLoad()`.
+ */
+
+class TwineScript : MonoBehaviour {
+  private var _instance : TwineScript;
+
+  function Awake() {
+    if (_instance) {
+      DestroyImmediate(gameObject);
+    } else {
+      DontDestroyOnLoad(gameObject);
+      _instance = this;
+    }
+  }
+}
