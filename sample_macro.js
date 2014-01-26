08fc8bbdf86fdff9b6019eefeff6af2599f06cfd
mildmojo <mildmojo@users.noreply.github.com>
2014-01-26 14:59:39 -0500

    Makes Unity/TwineGang integration more robust.
    Checks to make sure Unity/TwineGang stuff exists before trying to
    use it.
    
    Changes room name strategy to use 'room_<roomname>' tags instead
    of passage names, since multiple passages may be the same room.

--- sample_macro.js
+++ sample_macro.js
@@ -6,7 +6,9 @@ var emptyPassageName = tale.lookup('tags', 'empty')[0].title;
 var roomPassages = tale.lookup('tags', 'room');
 
 prerender.twineGang = function(div) {
-  TwineGang.arrive(this.title);
+  if (haveTwineGang()) {
+    TwineGang.arrive(this.title);
+  }
 };
 
 postrender.overrideRoomLinks = function(div) {
@@ -17,27 +19,53 @@ postrender.overrideRoomLinks = function(div) {
 
   jQuery(div).find('a.internalLink').each(function(_idx, el) {
     var passageName = el.getAttribute('id');
+    var passage = tale.get(passageName);
+    var isRoom = checkIsRoom(passage);
+    var roomName = getRoomName(passage);
     var $el = jQuery(el);
-    var isRoom = false;
-
-    roomPassages.forEach(function(roomPassage) {
-      if (roomPassage.title === passageName) {
-        isRoom = true;
-      }
-    });
 
     $el.on('click', function() {
-      TwineGang.click(passageName);
+      if (haveTwineGang()) {
+        TwineGang.click(passageName);
+      }
       $el.trigger('unity_actions');
     });
 
     $el.on('unity_actions', function() {
-      if (isRoom) {
+      if (isRoom && haveUnity()) {
         state.display(emptyPassageName);
-        Unity.SendMessage('NewRoom', passageName);
+        Unity.SendMessage('NewRoom', roomName);
       } else {
         state.display(passageName);
       }
     });
   });
 };
+
+function haveTwineGang() {
+  return typeof TwineGang !== 'undefined' && TwineGang;
+}
+
+function haveUnity() {
+  return typeof Unity !== 'undefined' && Unity;
+}
+
+function checkIsRoom(passage) {
+  var isRoom = false;
+  passage.tags.forEach(function(tag) {
+    if (tag === 'room') {
+      isRoom = true;
+    }
+  });
+  return isRoom;
+}
+
+function getRoomName(passage) {
+  var roomName = '';
+  passage.tags.forEach(function(tag) {
+    if (tag.match(/^room_/)) {
+      roomName = tag.replace(/^room_/, '');
+    }
+  });
+  return roomName;
+}
