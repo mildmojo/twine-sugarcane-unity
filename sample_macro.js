226e8b1875acade9fe85798ecc1ee95d61ae56f8
mildmojo <mildmojo@users.noreply.github.com>
2014-01-26 05:35:07 -0500

    Adds TwineGang network support.
    Adds support for TwineGang networked control between browsers.
    
    See: http://github.com/mildmojo/twine-gang

--- sample_macro.js
+++ sample_macro.js
@@ -1,11 +1,23 @@
 //requires jQuery
 
+// Also create a blank passage with the tag 'empty' on it.
+
 var emptyPassageName = tale.lookup('tags', 'empty')[0].title;
 var roomPassages = tale.lookup('tags', 'room');
 
-postrender.fixRoomLinks = function(div) {
+prerender.twineGang = function(div) {
+  TwineGang.arrive(this.title);
+};
+
+postrender.overrideRoomLinks = function(div) {
+  var divClone = div.cloneNode(true);
+  jQuery(divClone).insertAfter(div);
+  jQuery(div).remove();
+  div = divClone;
+
   jQuery(div).find('a.internalLink').each(function(_idx, el) {
     var passageName = el.getAttribute('id');
+    var $el = jQuery(el);
     var isRoom = false;
 
     roomPassages.forEach(function(roomPassage) {
@@ -14,11 +26,18 @@ postrender.fixRoomLinks = function(div) {
       }
     });
 
-    if (isRoom) {
-      jQuery(el).click(function(e) {
+    $el.on('click', function() {
+      TwineGang.click(passageName);
+      $el.trigger('unity_actions');
+    });
+
+    $el.on('unity_actions', function() {
+      if (isRoom) {
         state.display(emptyPassageName);
         Unity.SendMessage('NewRoom', passageName);
-      });
-    }
+      } else {
+        state.display(passageName);
+      }
+    });
   });
 };
